01b0481eae65763beb0f6934f9fff8d77d8c6612
Jay Wu <jay.wu@item.com>
2024-05-02 15:37:55 +0800

    feat:探索oauth2与client集成方案

--- playground/oauth2-consumer/server/app.js
+++ playground/oauth2-consumer/server/app.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-
+//
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
